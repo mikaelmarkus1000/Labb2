b6a4c386288b38b8cd99f20a300461f3bf513a52
mikaelmarkus1000 <mikaelmarkus1000@gmail.com>
2023-11-28 02:07:41 +0100

    Klar med koden
    Fixade mycket med koden. Saknar finslipning och komentarer men är helt funktionelt!

--- Labb2/Program.cs
+++ Labb2/Program.cs
@@ -2,44 +2,120 @@
 using System.Collections.Generic;
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("How many family members do you have?");
-        int familyNum = Convert.ToInt32(Console.ReadLine());
+    static void Main() { 
 
-        //Console.WriteLine("Meny");
-        //Console.WriteLine("1. Add Family members");
-        //Console.WriteLine("2. Print Family members");
-        //Console.WriteLine("3. Print Sum of family members ages");
-        //Console.WriteLine("4. Print the avrage of all family members");
+        string[] familyNames = new string[0];
+        int[] familyAges = new int[0];
 
 
-
-
-
-        string[] familyNames = new string[familyNum];
-        string[] familyAges = new string[familyNum];
-
-        for (int i = 0; i < familyNum; i++)
+    
+        
+        while (true)
         {
-            Console.WriteLine($"Enter the name of family member {i + 1}" );
-            string enteredName = Console.ReadLine();
-            familyNames[i] = enteredName;
-            Console.WriteLine($"Enter the age of family member {i + 1}");
-            string enteredAge = Console.ReadLine();
-            familyAges[i] = enteredAge;
+            Console.WriteLine("Meny");
+            Console.WriteLine("1. Add Family members");
+            Console.WriteLine("2. Print Family members");
+            Console.WriteLine("3. Print Sum of family members ages");
+            Console.WriteLine("4. Print the avrage of all family members");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice (1-5): ");
+            string choice = Console.ReadLine();
+
+            switch (choice) {
+
+                case "1":
+
+                    Console.WriteLine("Enter the number of family members: ");
+                    int familyNum = int.Parse(Console.ReadLine());
+
+                    familyNames = new string[familyNum];
+                    familyAges = new int[familyNum];
+
+                    for (int i = 0; i < familyNum; i++)
+                    {
+                        Console.WriteLine($"Enter the name of family member {i + 1}: ");                       
+                        familyNames[i] = Console.ReadLine();
+                        Console.WriteLine($"Enter the age of family {familyNames[i]}: ");
+                        familyAges[i] = int.Parse(Console.ReadLine());
+
+                    }
+                    break;
 
 
 
+
+
+
+                    
+                
+                        
+                        
+              case "2":
+
+
+                    Console.WriteLine("Family Members: ");
+                    for (int i = 0; i < familyNames.Length; i++)
+                    {
+                        Console.WriteLine($"Name: { familyNames[i]}");
+                        Console.WriteLine($"Age:  {familyAges[i]}");
+
+                    }
+
+                    break;
+
+                case "3":
+                    int result = 0;
+                    Console.WriteLine("----Combined age----");
+                    for (int i = 0; i < familyNames.Length; i++)
+                    {
+                        result += familyAges[i];
+                        
+
+
+                    }
+                    Console.WriteLine($"Sum of all ages:{result}");
+
+            break;
+            
+
+            case "4":
+
+                   
+                    {
+
+                        int sumForMedian = 0;
+                        for (int i = 0; i < familyAges.Length; i++)
+                        {
+                            sumForMedian += familyAges[i];
+                        }
+
+                        double median = (double)sumForMedian / familyAges.Length;
+                        Console.WriteLine($"Average age: {median:F2}");
+
+
+                    }
+
+
+                    break;
+
+
+
+                case "5":
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    return;
+                    
+                    
+               break;
+
+            }
         }
-        for (int i = 0; i < familyNum; i++)
-        {
-            Console.WriteLine(familyNames[i]);
-            Console.WriteLine(familyAges[i]);
-
-        }
+    
 
 
+
+
+        
 
 
 

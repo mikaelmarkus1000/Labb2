06656a00443fa9423c098a01b448a91f3179ab0a
Mikael <mikaelmarkus1000@gmail.com>
2023-11-29 10:50:49 +0100

    Fixat kommentarer och stavning.

--- Labb2/Labb2/Program.cs
+++ Labb2/Labb2/Program.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 class Program
 {
-    static void Main() { 
-
+    static void Main()
+    {
+        // Blanka arrayer som senare ska spara data!
         string[] familyNames = new string[0];
         int[] familyAges = new int[0];
 
 
-    
-        
+
+       //En evig while loop.
         while (true)
         {
+            //Print Print Print...
             Console.WriteLine("Meny");
             Console.WriteLine("1. Add Family members");
             Console.WriteLine("2. Print Family members");
@@ -21,39 +23,32 @@ class Program
 
             Console.Write("Enter your choice (1-5): ");
             string choice = Console.ReadLine();
-
-            switch (choice) {
+           //Switch 
+            switch (choice)
+            {
 
                 case "1":
-
+                    //Sparar mängden familje medlemar och gör storleken på array. 
                     Console.WriteLine("Enter the number of family members: ");
                     int familyNum = int.Parse(Console.ReadLine());
-
                     familyNames = new string[familyNum];
                     familyAges = new int[familyNum];
 
+
+                    // For loop som samlar och sparar data från användaren i rätt ordning. 
                     for (int i = 0; i < familyNum; i++)
                     {
-                        Console.WriteLine($"Enter the name of family member {i + 1}: ");                       
+                        Console.WriteLine($"Enter the name of family member {i + 1}: ");
                         familyNames[i] = Console.ReadLine();
-                        Console.WriteLine($"Enter the age of family {familyNames[i]}: ");
+                        Console.WriteLine($"Enter the age of {familyNames[i]}: ");
                         familyAges[i] = int.Parse(Console.ReadLine());
-
                     }
                     break;
 
 
+                case "2":
 
-
-
-
-                    
-                
-                        
-                        
-              case "2":
-
-
+                    //Printar ut familjemedlarar med en for loop använder familyNames.Length eftersom familyNum kan inte nås.  
                     Console.WriteLine("Family Members: ");
                     for (int i = 0; i < familyNames.Length; i++)
                     {
@@ -63,25 +58,22 @@ class Program
                     }
 
                     break;
-
+            // En till for-loop som += alla åldrar till en result.
                 case "3":
                     int result = 0;
                     Console.WriteLine("----Combined age----");
                     for (int i = 0; i < familyNames.Length; i++)
                     {
                         result += familyAges[i];
-                        
-
-
                     }
                     Console.WriteLine($"Sum of all ages:{result}");
 
-            break;
-            
+                    break;
 
-            case "4":
 
-                   
+                case "4":
+
+
                     {
 
                         int sumForMedian = 0;
@@ -92,30 +84,25 @@ class Program
 
                         double median = (double)sumForMedian / familyAges.Length;
                         Console.WriteLine($"Average age: {median:F2}");
-
-
                     }
 
 
                     break;
 
-
-
                 case "5":
                     Console.WriteLine("Exiting the program. Goodbye!");
                     return;
+
                     
-                    
-               break;
 
             }
         }
-    
 
 
 
 
-        
+
+
 
 
 
